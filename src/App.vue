6442fab6d38132e219ff30f534402ec0dad40764
julietaflux <julietaflux@gmail.com>
2020-02-13 21:50:56 -0300

    lala

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container v-if="showButton" class="fill-height justify-center" fluid>
+        <v-container fluid class="text-center font-weight-normal">
+          <v-btn
+            large
+            color="error"
+            @click.prevent="playPixies('http://cthulhu22.free.fr/Pixies%20-%20Wave%20Of%20Mutilation%20The%20Best%20Of%20Pixies%20(2004)%20320%20vtwin88cube/08.Hey.mp3')"
+          >{{ this.buttonTex }}</v-btn>
+        </v-container>
+      </v-container>
+
+      <v-container class="fill-height justify-center" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <HelloWorld v-if="showLyrics" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      showLyrics: false,
+      showButton: true,
+      buttonTex: `I'm ready <3`
+    };
+  },
+  methods: {
+    playPixies(hey) {
+      if (hey) {
+        this.showLyrics = true;
+        this.showButton = false;
+        var audio = new Audio(hey);
+        audio.play();
+      }
+    }
+  },
+  mounted() {
+    console.log("Te amo, Guillermo. Feliz San Valentín <3 PRRR");
+  }
+};
+</script>
+<style lang="css">
+#app {
+  font-family: "Courier New", serif;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container fluid class="text-center font-weight-normal">
+    <p>Hey</p>
+      <p><span class="highligh">Been trying to meet you</span></p>
+      <p>Hey</p>
+      <p>Must be a devil between us</p>
+      <p>Or whores in my head</p>
+      <p>Whores at my door</p>
+     <p> Whores in my bed</p>
+     <p> But hey</p>
+     <p> <span class="highligh">Where have you been?</span></p>
+     <p> <span class="highligh">If you go, i will surely die</span></p>
+      <p>We're chained</p>
+      <p><span class="highligh">'Uh' said the man to the lady</span></p>
+      <p><span class="highligh">'Uh' said the lady to the man she adored</span></p>
+      <p>And the whores like a choir</p>
+     <p> Go 'uh' all night</p>
+      <p>And mary ain't you tired of this?</p>
+     <p> Uh</p>
+      <p>Is</p>
+      <p>The</p>
+      <p>Sound</p>
+      <p>That the mother makes when the baby breaks</p>
+      <p><span class="highligh">We're chained</span></p>
+    </p>
+  </v-container>
+</template>
+
+
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+
+<style lang="sass">
+  .highligh
+    color: white
+    background-color: #ff5252 !important
+    padding-right: 5px
+    padding-left: 5px
+</style>
